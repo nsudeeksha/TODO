ebd0244df012ed1e66af38fa78e80f57610056e2
Sudeeksha Nooka <64000710@ep-student.org>
2021-02-11 12:08:58 -0600

    first file

---
+++ TODO/TODOApp.swift
@@ -0,0 +1,17 @@
+//hello
+//  TODOApp.swift
+//  TODO
+//
+//  Created by 64000710 on 2/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TODOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TODO/TasksFiles.swift
@@ -0,0 +1,18 @@
+//
+//  TasksFiles.swift
+//  TODO
+//
+//  Created by 64000710 on 2/7/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+struct Task : Identifiable {
+    var id = String()
+    var toDoItem = String()
+}
+class TaskStore : ObservableObject {
+    @Published var tasks = [Task]()
+}
